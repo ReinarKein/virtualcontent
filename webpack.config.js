e142d2e30d899ad43e15b5eceff21ad7fd8f2814
Valentin Krepkiy <valkrepkiy@gmail.com>
2017-04-06 15:18:25 +0300

    Make dist with webpack and babel;

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path    = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool : false,
+
+  entry   : './src/virtual_content.js',
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+
+  output: {
+    filename: 'virtualcontent.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins:[
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+
+};
